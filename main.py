0b904853e76d99fc77af0fec2290f4c68adf0868
LinHong <linlin.hong1@gmail.com>
2025-07-01 20:02:37 -0400

    Rules

--- main.py
+++ main.py
@@ -65,23 +65,38 @@ class PipsPuzzle:
         return True
     
 
-puzzle = PipsPuzzle(rows=5, cols=5)
+# puzzle = PipsPuzzle(rows=5, cols=5)
 
-# Sum of cells must equal 4
-puzzle.add_region([(0,0), (0,1), (1,0)], constraint_type='sum_eq', target_value=4)
+# print("Initial board:")
+# for i in range(puzzle.rows):
+#     for j in range(puzzle.cols):
+#         print(puzzle.board[i][j], end=' ')
+#     print()
 
-# Sum of cells must be less than 10
-puzzle.add_region([(1,2), (1,3), (2,2)], constraint_type='sum_lt', target_value=10)
+# # Sum of cells must equal 4
+# puzzle.add_region([(0,0), (0,1), (1,0)], constraint_type='sum_eq', target_value=4)
 
-# Sum of cells must be greater than 6
-puzzle.add_region([(2,3), (3,3), (3,2)], constraint_type='sum_gt', target_value=6)
+# # Sum of cells must be less than 10
+# puzzle.add_region([(1,2), (1,3), (2,2)], constraint_type='sum_lt', target_value=10)
 
-# All cells in the region must be equal (no target value needed!)
-puzzle.add_region([(3,0), (3,1)], constraint_type='all_eq')
+# # Sum of cells must be greater than 6
+# puzzle.add_region([(2,3), (3,3), (3,2)], constraint_type='sum_gt', target_value=6)
 
-# Place dominoes
-puzzle.place_domino((0,0), (0,1), 2, 2)
-puzzle.place_domino((1,0), (1,1), 2, 2)
+# # All cells in the region must be equal (no target value needed!)
+# puzzle.add_region([(3,0), (3,1)], constraint_type='all_eq')
 
-# Check puzzle validity
-print(puzzle.is_valid())  # True or False
+# # Place dominoes
+# puzzle.place_domino((0,0), (0,1), 2, 2)
+# puzzle.place_domino((1,0), (1,1), 2, 2)
+
+# print("Board after placing dominoes:")
+# for i in range(puzzle.rows):
+#     for j in range(puzzle.cols):
+#         print(puzzle.board[i][j], end=' ')
+#     print()
+
+# for region in puzzle.regions:
+#     print(region['cells'], region['constraint_type'], region['target_value'])
+
+# # Check puzzle validity
+# print(puzzle.is_valid())  # True or False
